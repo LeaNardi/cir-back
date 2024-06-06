9da1d163d6065788dcb2f3364a4676c741bb2688
Leandro <leandroenardi@gmail.com>
2024-06-06 20:18:11 -0300

    Profesional

---
+++ src/main/java/com/cir/cirback/entities/Especialidad.java
@@ -0,0 +1,27 @@
+package com.cir.cirback.entities;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity // This tells Hibernate to make a table out of this class
+@Data
+@NoArgsConstructor
+public class Especialidad {
+    @Id
+    private Integer especialidadId;
+
+    private String especialidad;
+}
